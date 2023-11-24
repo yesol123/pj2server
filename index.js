efbe28a23caa19a3a4084fe069d5d5de73b063d9
yesol123 <yesolg90@gmail.com>
2023-11-24 12:17:06 +0900

    d

--- index.js
+++ index.js
@@ -2,40 +2,42 @@ const express = require('express')
 const cors = require('cors')
 const fs = require('fs');
 const app = express()
-//fs 모듈 - readFileSync(): 읽기,WriteFileSync():쓰기
 
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json()) // 파서 등록! 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json()) // 파서 등록!
 
-app.get('api',(req,res)=>{
-  res.header("Access-Control-Allow-Origin","https://opendict.korean.go.kr/")
-  res.send(data)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const instant = axios.create({
+  baseURL: 'https://opendict.korean.go.kr',
+  params: { key: '4E4101E1F9C6B578FCE4D6CABE483676' },
+  timeout: 5000,
+  headers: { 'Content-Type': 'text/json', 'charset': 'UTF-8' }
 })
 
-
-app.get('/openapi', async function (req, res) { 
-  // const {url} = req.query;
-  
-  const a = await axios.get('https://opendict.korean.go.kr/api/search?key=4E4101E1F9C6B578FCE4D6CABE483676&target_type=search&req_type=json&part=word&q=구두')
-  console.log(a)
+app.get('/openapi', async function (req, res) {
+  const {randomWord} = req.query;
+  console.log(randomWord)
+  const a = await instant.get(`/api/search?target_type=search&req_type=json&part=word&q=${randomWord}`)
   res.send(a.data)
 })
 
-app.get('/', function (req, res) { 
-    const jD = fs.readFileSync('./gameData.json') 
-    res.send( JSON.parse(jD) )
-  })
+
+app.get('/', function (req, res) {
+  const jD = fs.readFileSync('./gameData.json')
+  res.send(JSON.parse(jD))
+})
 
 app.post('/insert', function (req, res) {
-    const jD = JSON.parse(fs.readFileSync('./gameData.json'))  //파서로 읽어올 수 있도록
-    fs.writeFileSync('./gameData.json', JSON.stringify([...jD,req.body])); //기존 게임데이터와 신규 게임데이터를 쌓아주기 
-    res.send("저장");
+  const jD = JSON.parse(fs.readFileSync('./gameData.json'))  //파서로 읽어올 수 있도록
+  fs.writeFileSync('./gameData.json', JSON.stringify([...jD, req.body])); //기존 게임데이터와 신규 게임데이터를 쌓아주기 
+  res.send("저장");
 })
 
 
 
-app.listen(3030)
+app.listen(3033)
